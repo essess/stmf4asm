8b2a1dbdd8c0b79179a730b19bd7bdc8df59859d
Sean Stasiak <sstasiak@gmail.com>
2012-01-20 17:58:11 -0700

    pll implementation checkpoint

--- startup.s
+++ startup.s
@@ -7,13 +7,16 @@
     .type       reset, function
     .global     reset
 reset:
+    ldr         lr, =reset          /**< TODO: something more creative to    */
+                                    /*         capture a stack underflow     */
+
+// TODO: wipeout code storage section
+//       8 bytes in bitband which gives us 256 different bits to set on failure
+
     bl          pll_init            /**< fire up pll to 168MHz               */
 
-// TODO: future: if proccess stack is != 0 then go ahead and set it up
-
-
-
-/**< comment */
+// TODO: wipe out bss sections
+// TODO: copy up init sections
 
 
 # loadup ram vectors
@@ -29,9 +32,12 @@ reset:
 1:
 # TODO: setup vtor
 
-# TODO: setup process stack if size > 0
+# TODO: wipe main stack (if configured to do so)
 
-# double check - stack should be as on reset.
+# TODO: setup process stack if size > 0
+#       wipe (if configured to do so)
+#       switch
+
     b           main
 /* 
     maybe do a bl here, and upon return
