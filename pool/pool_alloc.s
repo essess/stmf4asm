bb57876c4d25232dcff9426ef53ed9e041d60873
Sean Stasiak <sstasiak@gmail.com>
2012-02-10 15:26:45 -0700

    impl alloc, fully tested

--- pool/pool_alloc.s
+++ pool/pool_alloc.s
@@ -1,6 +1,11 @@
 /**
  * @public
- * <description/usage/assumptions>
+ * params:
+ *  r0 - ptr to head ptr
+ * retval:
+ *  r0 - ptr to unlinked pool block
+ * note:
+ *  is threadsafe
  */
     .section    .text
     .syntax     unified
@@ -10,6 +15,13 @@
     .type       pool_alloc, function
     .global     pool_alloc
 pool_alloc:
+    cpsid       i                   /**<                        start atomic */
+    ldr         r1, [r0]            /**< grab value of head                R */
+    cbz         r1, 1f              /**< is pool empty? (null head)          */
+    ldr         r2, [r1]            /**< no, get loc of next block in list M */
+    str         r2, [r0]            /**<     make it the new head          W */
+1:  cpsie       i                   /**<                          end atomic */
+    movs        r0, r1              /**< ref to unlinked block is retval     */
     bx          lr
     .size       pool_alloc, .-pool_alloc
 # -----------------------------------------------------------------------------
